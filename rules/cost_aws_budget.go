31b7315efa4e6fd26f4d1250598629af50b615ad
Koen Bolhuis <koen.bolhuis@gmail.com>
2024-05-30 20:21:10 +0200

    Implement rule for AWS budget

---
+++ rules/cost_aws_budget.go
@@ -0,0 +1,87 @@
+package rules
+
+import (
+	"github.com/terraform-linters/tflint-plugin-sdk/hclext"
+	"github.com/terraform-linters/tflint-plugin-sdk/logger"
+	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
+)
+
+// CostAwsBudgetRule checks whether ...
+type CostAwsBudgetRule struct {
+	tflint.DefaultRule
+}
+
+// NewCostAwsBudgetRule returns a new rule
+func NewCostAwsBudgetRule() *CostAwsBudgetRule {
+	return &CostAwsBudgetRule{}
+}
+
+// Name returns the rule name
+func (r *CostAwsBudgetRule) Name() string {
+	return "cost_aws_budget_exists"
+}
+
+// Enabled returns whether the rule is enabled by default
+func (r *CostAwsBudgetRule) Enabled() bool {
+	return true
+}
+
+// Severity returns the rule severity
+func (r *CostAwsBudgetRule) Severity() tflint.Severity {
+	return tflint.NOTICE
+}
+
+// Link returns the rule reference link
+func (r *CostAwsBudgetRule) Link() string {
+	return "https://search-rug.github.io/iac-cost-patterns/budget/"
+}
+
+// Check checks whether ...
+func (r *CostAwsBudgetRule) Check(runner tflint.Runner) error {
+	resources, err := runner.GetResourceContent("aws_budgets_budget", &hclext.BodySchema{
+		Attributes: []hclext.AttributeSchema{
+			{Name: "name"},
+		},
+	}, nil)
+	if err != nil {
+		return err
+	}
+
+	if len(resources.Blocks) == 0 {
+		tfBlocks, err := runner.GetModuleContent(&hclext.BodySchema{
+			Blocks: []hclext.BlockSchema{
+				{
+					Type: "terraform",
+					Body: &hclext.BodySchema{
+						Blocks: []hclext.BlockSchema{
+							{
+								Type: "required_providers",
+								Body: &hclext.BodySchema{
+									Blocks: []hclext.BlockSchema{
+										{
+											Type: "aws",
+											Body: &hclext.BodySchema{},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}, nil)
+		if err != nil {
+			return err
+		}
+
+		if len(tfBlocks.Blocks) < 1 {
+			logger.Warn("no terraform block defined")
+
+			return nil
+		}
+
+		runner.EmitIssue(r, "no budget defined", tfBlocks.Blocks[0].DefRange)
+	}
+
+	return nil
+}
---
+++ rules/cost_aws_budget_test.go
@@ -0,0 +1,70 @@
+package rules
+
+import (
+	"testing"
+
+	hcl "github.com/hashicorp/hcl/v2"
+	"github.com/terraform-linters/tflint-plugin-sdk/helper"
+)
+
+func Test_CostAwsBudget(t *testing.T) {
+	tests := []struct {
+		Name     string
+		Content  string
+		Expected helper.Issues
+	}{
+		{
+			Name: "issue found",
+			Content: `
+terraform {
+	required_providers {
+		aws {
+			source = "hashicorp/aws"
+			version = "5.51.1"
+		}
+	}
+}`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewCostAwsBudgetRule(),
+					Message: "no budget defined",
+					Range: hcl.Range{
+						Filename: "resource.tf",
+						Start:    hcl.Pos{Line: 2, Column: 1},
+						End:      hcl.Pos{Line: 2, Column: 10},
+					},
+				},
+			},
+		},
+		{
+			Name: "no issue found",
+			Content: `
+terraform {
+	required_providers {
+		aws {
+			source = "hashicorp/aws"
+			version = "5.51.1"
+		}
+	}
+}
+
+resource "aws_budgets_budget" "ec2_cost" {
+}`,
+			Expected: helper.Issues{},
+		},
+	}
+
+	rule := NewCostAwsBudgetRule()
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			runner := helper.TestRunner(t, map[string]string{"resource.tf": test.Content})
+
+			if err := rule.Check(runner); err != nil {
+				t.Fatalf("Unexpected error occurred: %s", err)
+			}
+
+			helper.AssertIssues(t, test.Expected, runner.Issues)
+		})
+	}
+}
